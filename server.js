74d82e482521b410c5a018fce93bdff79ddf4879
Leandro Verón <Leandro.veron92@gmail.com>
2025-09-07 01:49:35 -0300

    Update server.js
    agregación del dashboard al js

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/dashboard.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dashboard.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
+
 });
